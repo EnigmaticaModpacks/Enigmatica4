9ceef67a05aa0e24d3135ae968525e99a4cbbe01
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-08-04 21:11:52 +0200

    updated the server-run-script a bit.

--- Development/server-run-script.ps1
+++ Development/server-run-script.ps1
@@ -2,11 +2,14 @@
 The purpose of this script is to update and run Enigmatica 4 servers. 
 The server needs to be hooked up to the E4 github repository, and use the ATM server start script.
 This script should be placed in the root of the modpack instance.
+
+Clientside mods should be placed inside the $ClientMods array below, if any get added.
+This script is screaming DIY ^^'
 #>
 
 param(
     [PSObject]
-    $ClientMods = @("BetterAdvancements", "CosmeticArmorReworked", "EnchantmentDescriptions", "EquipmentTooltips", "FpsReducer", "inventorysorter", "Neat", "overloadedarmorbar", "swingthroughgrass", "theoneprobe", "ToastControl", "toughnessbar", "Xaeros_Minimap", "XaerosWorldMap")
+    $ClientMods = @("BetterAdvancements", "EnchantmentDescriptions", "EquipmentTooltips", "FpsReducer", "Neat", "overloadedarmorbar", "swingthroughgrass", "ToastControl", "toughnessbar", "Xaeros_Minimap", "XaerosWorldMap")
 )
 
 git pull
