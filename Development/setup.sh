f82f12e1e3e3d629194914fb519f15d1e822d0e8
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-09 11:11:38 +0200

    Deleting files to readd some of them, and ignore some

--- Development/setup.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-
-echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar InstanceSync.jar" >> .git/hooks/post-merge
-
-echo "Done setting up hooks"
-echo "Running InstanceSync"
-
-java -jar InstanceSync.jar
