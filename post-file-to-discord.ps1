ce479c48ca7fd6514ea7980e9e3c451fe6560a5e
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-09 11:55:10 +0200

    Update post-file-to-discord.ps1

--- post-file-to-discord.ps1
+++ post-file-to-discord.ps1
@@ -9,5 +9,5 @@ if (Test-Path $secretPath) {
 else 
 {
     .\TwitchExportBuilder.exe -n "$CLIENT_FILENAME" -p "$MODPACK_VERSION"
-	curl --url "https://discordapp.com/api/webhooks/$env:WEBHOOK_ID/$env:WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"$CLIENT_FILENAME-$MODPACK_VERSION.zip" --progress-bar
+	curl --url "https://discordapp.com/api/webhooks/$env:WEBHOOK_ID/$env:WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"Enigmatica4-0.1.1.zip" --progress-bar
 }
