0fd1978151f81c90033052a4e286f0addd27ce91
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-09 12:32:39 +0200

    Update post-file-to-discord.ps1

--- post-file-to-discord.ps1
+++ post-file-to-discord.ps1
@@ -3,11 +3,9 @@ $secretPath = "$PSScriptRoot\secrets.ps1"
 
 if (Test-Path $secretPath) {
 	. $secretPath
-    .\TwitchExportBuilder.exe -n "$CLIENT_FILENAME" -p "$MODPACK_VERSION"
 	curl.exe --url "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"$CLIENT_FILENAME-$MODPACK_VERSION.zip" --progress-bar
 } 
 else 
 {
-    .\TwitchExportBuilder.exe -n "$CLIENT_FILENAME" -p "$MODPACK_VERSION"
 	curl --url "https://discordapp.com/api/webhooks/$env:WEBHOOK_ID/$env:WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"Enigmatica4-0.1.1.zip" --progress-bar
 }
