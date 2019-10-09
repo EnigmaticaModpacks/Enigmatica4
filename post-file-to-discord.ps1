6bc520c8334a3905811786e41d884d1ca2c228fc
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-09 21:53:55 +0200

    Added mods, disabled Azure, disabled Azure parts of post-to-discord script

--- post-file-to-discord.ps1
+++ post-file-to-discord.ps1
@@ -1,11 +1,34 @@
-. "$PSScriptRoot\settings.ps1"
-$secretPath = "$PSScriptRoot\secrets.ps1"
+# . "$PSScriptRoot\settings.ps1"
+# $SecretPath = "$PSScriptRoot\secrets.ps1"
+# . "$PSScriptRoot\secrets.ps1"
+# $CurlUrl = "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN"
+# $InvUrl = "https://discordapp.com/api/webhooks/$env:WEBHOOK_ID/$env:WEBHOOK_TOKEN"
+# $FilePath = "Enigmatica4-0.1.1.zip"
 
-if (Test-Path $secretPath) {
-	. $secretPath
-	curl.exe --url "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"$CLIENT_FILENAME-$MODPACK_VERSION.zip" --progress-bar
-} 
-else 
-{
-	curl.exe --url "https://discordapp.com/api/webhooks/$env:WEBHOOK_ID/$env:WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"Enigmatica4-0.1.1.zip" --progress-bar
-}
+
+# $payload = [PSCustomObject]@{
+
+#     content = file
+
+# }
+
+# Invoke-RestMethod -Uri $CurlUrl -Method Post -Body ($payload | ConvertTo-Json)
+
+# -Headers @{ ContentType = 'multipart/form-data'}
+
+# if (Test-Path $SecretPath) {
+# 	. $SecretPath
+#  	curl.exe --url "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN" -F ContentType="multipart/form-data" -F content=@"$CLIENT_FILENAME-$MODPACK_VERSION.zip" --progress-bar
+# } 
+# else 
+# {
+# 	Invoke-RestMethod -Uri $CurlUrl -Method Post -Headers "{ 'ContentType': 'multipart/form-data' }" -InFile $FilePath
+# 	curl.exe --url $InvUrl -F ContentType="multipart/form-data" -F content=@"Enigmatica4-0.1.1.zip" --progress-bar
+# }
+
+. "$PSScriptRoot\settings.ps1"
+. "$PSScriptRoot\secrets.ps1"
+$CurlUrl = "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN"
+$FilePath = "Enigmatica4-0.1.1.zip"
+
+curl.exe --url @CurlUrl -F ContentType="multipart/form-data" -F content=@"$CLIENT_FILENAME-$MODPACK_VERSION.zip" --progress-bar
