c67a498d1cafac5452d61f224d132c61104b1bda
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-31 22:42:43 +0100

    Woops, forgot a variable in remove-client-mods.ps1

--- development/include-in-server-files/remove-client-mods.ps1
+++ development/include-in-server-files/remove-client-mods.ps1
@@ -21,12 +21,13 @@ param(
 		"XaerosWorldMap")
 )
 
+$ModFolder = "$PSScriptRoot/mods"
 
 Get-ChildItem $ModFolder -Name -Filter  "*.jar" | ForEach-Object {
 	$Mod = $_.toLower()
 	foreach ($ClientMod in $ClientMods) {
 		if ($Mod.StartsWith($ClientMod.toLower())) {
-			Remove-Item "$Modfolder/$mod" -Force
+			Remove-Item "$Modfolder/$Mod" -Force
 		}
 	}
 }
