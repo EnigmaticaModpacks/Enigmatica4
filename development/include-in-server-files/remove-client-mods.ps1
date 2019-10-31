fd15bd127ea4306adc24923e5131d7375e81c370
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-10-31 20:14:34 +0100

    Added a script to remove client side mods from servers.

---
+++ development/include-in-server-files/remove-client-mods.ps1
@@ -0,0 +1,32 @@
+param(
+	[PSObject]
+	$ClientMods = @(
+		"AppleSkin", 
+		"BetterAdvancements",
+		"CraftingTweaks", 
+		"DefaultOptions", 
+		"EnchantmentDescriptions", 
+		"EquipmentTooltips", 
+		"FpsReducer", 
+		"JustEnoughResources",
+		"LLOverlayReloaded", 
+		"MouseTweaks",
+		"nmdar_", 
+		"Neat", 
+		"overloadedarmorbar", 
+		"swingthroughgrass", 
+		"ToastControl", 
+		"toughnessbar", 
+		"Xaeros_Minimap", 
+		"XaerosWorldMap")
+)
+
+
+Get-ChildItem $ModFolder -Name -Filter  "*.jar" | ForEach-Object {
+	$Mod = $_.toLower()
+	foreach ($ClientMod in $ClientMods) {
+		if ($Mod.StartsWith($ClientMod.toLower())) {
+			Remove-Item "$Modfolder/$mod" -Force
+		}
+	}
+}
