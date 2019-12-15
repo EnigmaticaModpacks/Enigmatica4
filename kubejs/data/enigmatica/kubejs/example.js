374aa25c4084d0b9d4ffd594b219e872c889168a
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-12-15 19:53:15 +0100

    Oredicted a few Basalt stones with KubeJS to learn

--- kubejs/data/enigmatica/kubejs/example.js
+++
@@ -1,2 +0,0 @@
-//https://kubejs.latvian.dev/
-//console.info('Hello, World! (You will see this line every time you start server or run /reload)')
--- kubejs/data/enigmatica/kubejs/tags/basalt.js
+++ kubejs/data/enigmatica/kubejs/tags/basalt.js
@@ -1,3 +1,7 @@
+server.logAddedRecipes = true
+server.logRemovedRecipes = true
+
+// TODO: This will be refactored to `server.datapack.tags.items` in a future version of KubeJS.
 events.listen('server.datapack.tags', function (event) {
     event.tags.items.get('forge:stones/basalt').add('quark:basalt', 'bluepower:basalt', 'bluepower:basalt_brick');
 })
