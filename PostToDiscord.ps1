4d9b2991232f1a8e994dca58edadda5c6d10b15c
NielsPilgaard <niels.pilgaard@hotmail.com>
2020-04-12 21:08:48 +0200

    Dev files

--- PostToDiscord.ps1
+++ PostToDiscord.ps1
@@ -4,3 +4,4 @@ $CurlUrl = "https://discordapp.com/api/webhooks/$WEBHOOK_ID/$WEBHOOK_TOKEN"
 $FilePath = "$CLIENT_FILENAME-$MODPACK_VERSION.zip"
 
 curl.exe --url $CurlUrl -F ContentType="multipart/form-data" -F content=@$FilePath --progress-bar
+pause
