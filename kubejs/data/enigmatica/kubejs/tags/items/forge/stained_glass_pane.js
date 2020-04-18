a5a81caf2ea864822803fea7c30e168ec9f82625
NielsPilgaard <niels.pilgaard@hotmail.com>
2020-04-18 21:43:33 +0200

    Remade a bunch of vanilla recipes that don't take tag dyes, fixes #371

---
+++ kubejs/data/enigmatica/kubejs/tags/items/forge/stained_glass_pane.js
@@ -0,0 +1,5 @@
+events.listen('server.datapack.tags.items', function (event) {
+    for (var i = 0; i < colors.length; i++) {
+        event.get('forge:stained_glass_pane').add('minecraft:' + colors[i] + '_stained_glass_pane');
+    }
+})
