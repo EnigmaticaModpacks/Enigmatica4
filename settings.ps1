0f5e2ee3212fe98645ef2fb1c8c62ed0ba0f029d
NielsPilgaard <niels.pilgaard@hotmail.com>
2020-04-19 10:59:34 +0200

    Updated Forge

--- settings.ps1
+++ settings.ps1
@@ -45,7 +45,7 @@ $MODPACK_VERSION = "0.5.3"
 # Only used by the Changelog Generator
 $LAST_MODPACK_VERSION = "0.5.2"
 
-$FORGE_VERSION=28.2.3
+$FORGE_VERSION=28.2.4
 
 $CLIENT_FILENAME = "Enigmatica4"
 
@@ -62,7 +62,7 @@ $CLIENT_CHANGELOG_TYPE = "markdown"
 
 # Must be a single string. Use Powershell escaping for new lines etc. New line is `n and indent is `t
 $CLIENT_CHANGELOG = "### Enigmatica 4 v$MODPACK_VERSION <br /><br />" +
-					"Using Forge-1.14.4-28.2.3 <br />" +
+					"Using Forge-1.14.4-28.2.4 <br />" +
 					"[Mod Updates](https://github.com/NillerMedDild/Enigmatica4/blob/master/changelogs/CHANGELOG_MODS_$MODPACK_VERSION.txt) <br /> " +
 					"[Changelog](https://github.com/NillerMedDild/Enigmatica4/blob/master/changelogs/CHANGELOG.md) "
 
